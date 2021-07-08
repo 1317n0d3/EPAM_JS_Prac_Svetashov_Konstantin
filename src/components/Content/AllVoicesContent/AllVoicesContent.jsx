4838f820bd5dba8135fe6d2eb7340bb9acb04f64
klltx <kostya.svetashov.01@gmail.com>
2021-07-08 13:38:26 +0400

    fix: render messages

--- src/components/Content/AllVoicesContent/AllVoicesContent.jsx
+++ src/components/Content/AllVoicesContent/AllVoicesContent.jsx
@@ -4,22 +4,13 @@ const AllVoicesContent = () => {
   const [data, setData] = useState([]),
     messages = data.map((m, index) => <li key={ index } >{ m.timeStamp }</li>);
 
-  // const convertToAudio = () => {
-  //   if(data.length > 1){
-  //     const audioBlob = new Blob(data[3].audioBlob[0].data, {type: 'text/plain'});
-  //     const audioUrl = URL.createObjectURL(audioBlob);
-  //     const audio = new Audio(audioUrl);
-  //     return audio;
-  //   }
-  // }
-
   useEffect(() => {
     fetch('https://voicy-speaker.herokuapp.com/voices')
       .then(response => response.json())
       .then(json => setData(json))
   }, []);
 
-  const renderMessages = (count, messages) => messages.slice(messages.length - count, messages.length);
+  const renderMessages = (count, messages) => (count < messages.length) ? messages.slice(messages.length - count, messages.length) : messages;
 
   return (
     <section className='current-control'>
