aca333fe2dbb543177339a99cf39e3c6fde173a3
klltx <kostya.svetashov.01@gmail.com>
2021-07-08 15:00:29 +0400

    style: all voices

--- src/components/Content/AllVoicesContent/AllVoicesContent.jsx
+++ src/components/Content/AllVoicesContent/AllVoicesContent.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const AllVoicesContent = () => {
   const [data, setData] = useState([]),
-    messages = data.map((m, index) => <li key={ index } >{ m.timeStamp }</li>);
+    messages = data.map((message, index) => <li key={ index }><span>{message.timeStamp.slice(4, 21)}</span><audio controls loop src={ createAudioUrl(message.audioBlob[0].data) } type="audio/mpeg"></audio></li>);
 
   useEffect(() => {
     fetch('https://voicy-speaker.herokuapp.com/voices')
@@ -10,11 +10,17 @@ const AllVoicesContent = () => {
       .then(json => setData(json))
   }, []);
 
+  function createAudioUrl(audioChunks) {
+    const audioBlob = new Blob(audioChunks, {type : 'audio/ogg'});
+    const audioUrl = URL.createObjectURL(audioBlob);
+    // const audio = new Audio(audioUrl);
+    return audioUrl;
+  }
+
   const renderMessages = (count, messages) => (count < messages.length) ? messages.slice(messages.length - count, messages.length) : messages;
 
   return (
-    <section className='current-control'>
-      <span>Active: all devices</span>
+    <section className='current-control all-voices'>
       <ul>
         { renderMessages(5, messages) }
       </ul>
