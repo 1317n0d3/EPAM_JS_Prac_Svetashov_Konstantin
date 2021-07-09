616eaf6b3011a59441295fb7af1d12f865ea29b8
klltx <kostya.svetashov.01@gmail.com>
2021-07-09 21:08:17 +0400

    server feat: run react project build from server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-const app = require("express")();
+const express = require("express");
 const cors = require('cors');
+const path = require('path');
+const app = express();
 
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
